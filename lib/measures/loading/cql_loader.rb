9e62f98d8bfd940edf9c10bd4080090edbb22df4
Elliott <aelliott@mm198210-pc.mitre.org>
2017-02-22 09:14:12 -0500

    BONNIE-216 Adds logic for line exclusion

--- lib/measures/loading/cql_loader.rb
+++ lib/measures/loading/cql_loader.rb
@@ -116,6 +116,7 @@ module Measures
           }
         )
         elm_xml = request.execute
+        elm_xml.gsub!(/\n/, '')
         elm_xml = CQL_ELM::Parser.parse(elm_xml)
         return parse_elm_response(elm_json), elm_xml
       rescue RestClient::BadRequest => e
