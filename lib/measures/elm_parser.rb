4fb10291d555d0e2a1a98ff0cd1388ec760a4806
Elliott <aelliott@mm198210-pc.mitre.org>
2017-07-11 13:17:40 -0400

    BONNIE-706 Adds comments and removes extraneous content to elm_parser.

--- lib/measures/elm_parser.rb
+++ lib/measures/elm_parser.rb
@@ -1,14 +1,15 @@
 module CQL_ELM
   class Parser
+    #Fields are combined with the refId to find elm node that corrosponds to the current annotation node.
     @fields = ['expression', 'operand', 'suchThat']
-    @curDefine
+    
     def self.parse(elm_xml)
       ret = {
         statements: [],
         identifier: {}
       }
-#      ret = Nokogiri::HTML.fragment('<div id="statements"></div>')
       @doc = Nokogiri::XML(elm_xml)
+      #extract library identifier data
       ret[:identifier][:id] = @doc.css("identifier").attr("id").value()
       ret[:identifier][:version] = @doc.css("identifier").attr("version").value()
       
@@ -23,6 +24,8 @@ module CQL_ELM
       ret
     end
     
+    #Recursive function that traverses the annotation tree and constructs a representation
+    #that will be compatible with the front end.
     def self.parse_node(node, parent_type=nil)
       parent_type = parent_type.downcase unless parent_type.nil?
       ret = {
@@ -31,26 +34,33 @@ module CQL_ELM
       define_name = nil
       node.children.each do |child|
         begin
+          #Nodes with the 'a' prefix are not leaf nodes
           if child.namespace.respond_to?(:prefix) && child.namespace.prefix == 'a'
             ref_node = nil
             node_type = nil
+            #Tries to pair the current annotation node with an elm node.
             @fields.each do |field|
               ref_node ||= @doc.at_css(field + '[localId="'+child['r']+'"]') unless child['r'].nil?
             end
+            #Tries to extract the current node's type.
             node_type = ref_node['xsi:type'] unless ref_node.nil?
+            #Parses the current child recursively. child_define_name will bubble up to indicate which
+            #statement is currently being traversed.
             node, child_define_name = parse_node(child, node_type)
             node[:node_type] = node_type  unless node_type.nil?
             node[:ref_id] = child['r'] unless child['r'].nil?
             define_name = child_define_name unless child_define_name.nil? 
             ret[:children] << node
           else
+            #Cull pure whitespace nodes.
             if (/^\n\s+$/ =~ child.to_html).nil?
+              #Determine if the current leaf is the one that contains the define name
+              #If so, start bubbling it up.
+              #There will only be a single define node in the tree.
               if (/^define/ =~ child.to_html)
                 define_name = child.to_html.split("\"")[1]
               end
               clause = {
-                #text: child.to_html.gsub("&gt;", ">").gsub("&lt;", "<").gsub("&quot;", '"').gsub("&amp;", "&").gsub("&apos;", "'")
-                #text: child.to_html.gsub("\n", '<br/>').gsub(/\s/, "&#160;")
                 text: child.to_html
               }
               clause[:ref_id] = child['r'] unless child['r'].nil?
