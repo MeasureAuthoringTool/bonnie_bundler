358e315e47df3078ba688cf85c7fb9db9de851f2
Elliott <aelliott@mm198210-pc.mitre.org>
2017-07-14 12:04:13 -0400

    Remove begin/rescue, break out functions.

--- lib/measures/elm_parser.rb
+++ lib/measures/elm_parser.rb
@@ -14,6 +14,7 @@ module CqlElm
       ret[:identifier][:id] = @doc.css("identifier").attr("id").value()
       ret[:identifier][:version] = @doc.css("identifier").attr("version").value()
       
+      #extracts the fields of type "annotation" and their children.
       annotations = @doc.css("annotation")
       annotations.each do |node|
           node, define_name = parse_node(node)
@@ -33,53 +34,58 @@ module CqlElm
       }
       define_name = nil
       node.children.each do |child|
-        begin
-          #Nodes with the 'a' prefix are not leaf nodes
-          if child.namespace.respond_to?(:prefix) && child.namespace.prefix == 'a'
-            ref_node = nil
-            node_type = nil
-            #Tries to pair the current annotation node with an elm node.
-            @fields.each do |field|
-              ref_node ||= @doc.at_css(field + '[localId="'+child['r']+'"]') unless child['r'].nil?
+        #Nodes with the 'a' prefix are not leaf nodes
+        if child.namespace.respond_to?(:prefix) && child.namespace.prefix == 'a'
+          node_type = extract_node_type(child)
+          #Parses the current child recursively. child_define_name will bubble up to indicate which
+          #statement is currently being traversed.
+          node, child_define_name = parse_node(child)
+          node[:node_type] = node_type  unless node_type.nil?
+          node[:ref_id] = child['r'] unless child['r'].nil?
+          define_name = child_define_name unless child_define_name.nil? 
+          ret[:children] << node
+        else
+          #Cull pure whitespace nodes.
+          if (/^\n\s+$/ =~ child.to_html).nil?
+            #Determine if the current leaf is the one that contains the define name
+            #If so, start bubbling it up.
+            #There will only be a single define node in the tree.
+            if (/^define/ =~ child.to_html)
+              define_name = child.to_html.split("\"")[1]
             end
-            #Tries to extract the current node's type.
-            node_type = ref_node['xsi:type'] unless ref_node.nil?
-            #Parses the current child recursively. child_define_name will bubble up to indicate which
-            #statement is currently being traversed.
-            node, child_define_name = parse_node(child)
-            node[:node_type] = node_type  unless node_type.nil?
-            node[:ref_id] = child['r'] unless child['r'].nil?
-            define_name = child_define_name unless child_define_name.nil? 
-            ret[:children] << node
-          else
-            #Cull pure whitespace nodes.
-            if (/^\n\s+$/ =~ child.to_html).nil?
-              #Determine if the current leaf is the one that contains the define name
-              #If so, start bubbling it up.
-              #There will only be a single define node in the tree.
-              if (/^define/ =~ child.to_html)
-                define_name = child.to_html.split("\"")[1]
-              end
-              clause = {
-                text: child.to_html
-              }
-              #TODO: This is ugly, hopefully the stuff we get from the translation service will give good data
-              if @previousNoTrailingSpaceNotPeriod && (/\.$/ =~ clause[:text]).nil? && (/^\s/ =~ clause[:text]).nil?
-                clause[:text] = " " + clause[:text]
-              end
-              @previousNoTrailingSpaceNotPeriod = false
-              if (/\s$/ =~ clause[:text]).nil? && (/\.$/ =~ clause[:text]).nil?
-                @previousNoTrailingSpaceNotPeriod = true
-              end
-              clause[:ref_id] = child['r'] unless child['r'].nil?
-              ret[:children] << clause
-            end
+            clause = {
+              text: child.to_html
+            }
+            add_whitespace(clause)
+            clause[:ref_id] = child['r'] unless child['r'].nil?
+            ret[:children] << clause
           end
-        rescue
-          puts e
         end
       end
       return ret, define_name
+    end
+    
+    def self.extract_node_type(child)
+      ref_node = nil
+      node_type = nil
+      #Tries to pair the current annotation node with an elm node.
+      @fields.each do |field|
+        ref_node ||= @doc.at_css(field + '[localId="'+child['r']+'"]') unless child['r'].nil?
+      end
+      #Tries to extract the current node's type.
+      node_type = ref_node['xsi:type'] unless ref_node.nil?
+      node_type
+    end
+    
+    def self.add_whitespace(clause)
+      #TODO: This is ugly, hopefully the stuff we get from the translation service will give good data
+      if @previousNoTrailingSpaceNotPeriod && (/\.$/ =~ clause[:text]).nil? && (/^\s/ =~ clause[:text]).nil?
+        clause[:text] = " " + clause[:text]
+      end
+      @previousNoTrailingSpaceNotPeriod = false
+      if (/\s$/ =~ clause[:text]).nil? && (/\.$/ =~ clause[:text]).nil?
+        @previousNoTrailingSpaceNotPeriod = true
+      end
     end
   end
 end
