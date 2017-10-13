fb1ae52cb6983b04ddc795cc03a5094cbc8757b9
Matt Mayer <mmayer@mitre.org>
2017-10-13 14:29:27 -0400

    Re add test, converted to use a CQL fixture so that James will approve my PR

---
+++ test/unit/measure_complexity_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class MeasureComplexityTest < ActiveSupport::TestCase
+
+  setup do
+    @cql_mat_export = File.new File.join('test', 'fixtures', 'BCS_v5_0_Artifacts.zip')
+  end
+
+  test "Loading a CQL Mat export zip file, with VSAC credentials" do
+    VCR.use_cassette("valid_vsac_response") do
+      dump_db
+      user = User.new
+      user.save
+      measure_details = { 'episode_of_care'=> false }
+      Measures::CqlLoader.load(@cql_mat_export, user, measure_details, ENV['VSAC_USERNAME'], ENV['VSAC_PASSWORD']).save
+      assert_equal 1, CqlMeasure.all.count
+      measure = CqlMeasure.all.first
+      assert_equal 10, measure.complexity["variables"].length
+      assert_equal [{"name"=>"Patient", "complexity"=>1},
+                    {"name"=>"SDE Ethnicity", "complexity"=>1},
+                    {"name"=>"SDE Payer", "complexity"=>1},
+                    {"name"=>"SDE Race", "complexity"=>1},
+                    {"name"=>"SDE Sex", "complexity"=>1},
+                    {"name"=>"Initial Pop", "complexity"=>2},
+                    {"name"=>"Num", "complexity"=>1},
+                    {"name"=>"Double Unilateral Mastectomy", "complexity"=>1},
+                    {"name"=>"Denom", "complexity"=>3},
+                    {"name"=>"Denom Excl", "complexity"=>2}], measure.complexity["variables"]
+    end
+  end
+
+end
