83c1b2735f6b51e4b34b9b8228eec78126c981b6
Matt Mayer <mmayer@mitre.org>
2017-10-19 11:55:27 -0400

    Add test for adding draft value sets

--- test/unit/load_mat_export_test.rb
+++ test/unit/load_mat_export_test.rb
@@ -7,6 +7,27 @@ class LoadMATExportTest < ActiveSupport::TestCase
     @cql_mat_export = File.new File.join('test', 'fixtures', 'BCS_v5_0_Artifacts.zip')
     @cql_mat_5_4_export = File.new File.join('test', 'fixtures', 'CMS158_v5_4_Artifacts.zip')
     @cql_multi_library_mat_export = File.new File.join('test', 'fixtures', 'bonnienesting01_fixed.zip')
+    @cql_draft_measure_mat_export = File.new File.join('test', 'fixtures', 'DRAFT_CMS2_CQL.zip')
+  end
+
+  test "Loading a CQL Mat export zip file including draft, with VSAC credentials" do
+    VCR.use_cassette("valid_vsac_response_includes_draft") do
+      dump_db
+      user = User.new
+      user.save
+      measure_details = { 'episode_of_care'=> false }
+      Measures::CqlLoader.load(@cql_draft_measure_mat_export, user, measure_details, ENV['VSAC_USERNAME'], ENV['VSAC_PASSWORD'], overwrite_valuesets=false, cache=false, includeDraft=true, ticket_granting_ticket=nil).save
+      assert_equal 1, CqlMeasure.all.count
+      measure = CqlMeasure.all.first
+      assert_equal "Screening for Depression", measure.title
+      assert_equal "40280582-5B4D-EE92-015B-827458050128", measure.hqmf_id
+      assert_equal 1, measure.populations.size
+      assert_equal 5, measure.population_criteria.keys.count
+      # Assert value_set versions are all "Draft-"
+      for value_set in measure.value_sets
+        assert_equal "Draft-", value_set["version"]
+      end
+    end
   end
 
   test "Loading a CQL Mat export zip file, with VSAC credentials" do
