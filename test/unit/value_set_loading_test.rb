64c12d73e8a095e399501a33e97b030d35aa59a0
Chris Hossenlopp <hossenlopp@mitre.org>
2018-04-02 09:35:49 -0400

    Added tests for release and measure_defined options. changed one test to measure_defined with backup_profile

--- test/unit/value_set_loading_test.rb
+++ test/unit/value_set_loading_test.rb
@@ -55,15 +55,45 @@ class ValueSetLoadingTest < ActiveSupport::TestCase
   end
 
   # TODO: re-evaluate test. comment suggests a situation that will not be an option to the user
+  # unless using a backup profile call will be the route taken
   test 'Loading without IncludeDraft and no Profile or Version' do
     # Expects that default profile will be used
     VCR.use_cassette("vs_loading_no_profile_version") do
       mat_file = File.new File.join("test", "fixtures", "vs_loading", "DocofMeds_v5_1_Artifacts.zip")
       measure_details = {}
-      measure = Measures::CqlLoader.load(mat_file, @user, measure_details, { profile: APP_CONFIG['vsac']['default_profile'] }, get_ticket_granting_ticket)
+      measure = Measures::CqlLoader.load(mat_file, @user, measure_details, { measure_defined: true, backup_profile: APP_CONFIG['vsac']['default_profile'] }, get_ticket_granting_ticket)
       measure.value_sets.each do |vs|
         if vs.oid == "2.16.840.1.113883.3.600.1.1834"
           assert_equal 154, vs.concepts.count
+        end
+      end
+    end
+  end
+
+  # TODO: figure out if the backup_profile option should be kept or not
+  test 'Loading with measure_defined and no backup_profile' do
+    # Expects that no vsac options will be used. just bare query with only oid parameter
+    VCR.use_cassette("vs_loading_meausre_defined_no_backup_profile") do
+      mat_file = File.new File.join("test", "fixtures", "vs_loading", "DocofMeds_v5_1_Artifacts.zip")
+      measure_details = {}
+      measure = Measures::CqlLoader.load(mat_file, @user, measure_details, { measure_defined: true }, get_ticket_granting_ticket)
+      measure.value_sets.each do |vs|
+        if vs.oid == "2.16.840.1.113883.3.600.1.1834"
+          assert_equal 175, vs.concepts.count
+        end
+      end
+    end
+  end
+
+  test 'Loading with release' do
+    # Expects that the provided release will be used
+    VCR.use_cassette("vs_loading_release") do
+      mat_file = File.new File.join("test", "fixtures", "vs_loading", "DocofMeds_v5_1_Artifacts.zip")
+      measure_details = {}
+      measure = Measures::CqlLoader.load(mat_file, @user, measure_details, { release: 'eCQM Update 2018 EP-EC and EH' }, get_ticket_granting_ticket)
+      measure.value_sets.each do |vs|
+        if vs.oid == "2.16.840.1.113883.3.600.1.1834"
+          assert_equal 162, vs.concepts.count
         end
       end
     end
@@ -75,7 +105,7 @@ class ValueSetLoadingTest < ActiveSupport::TestCase
       mat_file = File.new File.join("test", "fixtures", "vs_loading", "DocofMeds_v5_1_Artifacts.zip")
       measure_details = {}
       exception = assert_raise Measures::VSACException do
-        measure = Measures::CqlLoader.load(mat_file, "fake user", measure_details, { profile: APP_CONFIG['vsac']['default_profile'] }, get_ticket_granting_ticket)
+        measure = Measures::CqlLoader.load(mat_file, "fake user", measure_details, { measure_defined: true, backup_profile: APP_CONFIG['vsac']['default_profile'] }, get_ticket_granting_ticket)
       end
       assert_equal 'Error Loading Value Sets from VSAC: undefined method `id\' for "fake user":String', exception.message
     end
